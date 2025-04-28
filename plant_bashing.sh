10f3fbd5f71eda6c83692ecc33c17652376deec1
Weibo0123 <Weibolin322@gmail.com>
2025-04-28 15:22:28 -0500

    ask users if they want to plant a new seed

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -7,3 +7,22 @@ read name
 echo "Hello, $name! Nice to meet you"
 
 sleep 5
+
+while true; do
+echo "Do you want to plant a new seed? (yes/no)"
+read choice
+
+if [ "$choice" == "yes" ]; then
+    # If the user chooses 'yes', let them know they have planted a seed
+    echo "You have dug a hole and planted a small seed the size of a grain of rice."
+    break
+elif [ "$choice" == "no" ]; then
+    echo "You chose not to plant a seed. Exiting in 3 seconds..."
+    sleep 3
+    break
+else
+	echo "You can only anwser yes or no"
+fi
+done
+
+echo "Goodbye"
