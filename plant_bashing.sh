b7401df645a482397215349ef709e89faa825e3a
Weibo0123 <Weibolin322@gmail.com>
2025-04-29 15:11:59 -0500

    User can plant seed and wait for the germiantion

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -15,6 +15,26 @@ read choice
 if [ "$choice" == "yes" ]; then
     # If the user chooses 'yes', let them know they have planted a seed
     echo "You have dug a hole and planted a small seed the size of a grain of rice."
+    sleep 3
+    echo "In the Computer's digital world, the ime moves mush faster than it does outside in the reality.Do you want to wait for one day?"
+    read wait
+
+    if [ "$wait" == "yes" ]; then
+        echo "wait for one days"
+        sleep 3
+        echo "Nothing Happen, do you want to wait for one more days?"
+        read wait2
+        if [[ "$wait2" == "yes" ]]; then
+            echo "The seed germinated over night!"
+        else 
+            break
+        fi
+    elif [ "$wait" == "no" ]; then
+    echo "You chose not to wait. Exiting in 3 seconds..."
+    sleep 3
+    break
+fi
+
     break
 elif [ "$choice" == "no" ]; then
     echo "You chose not to plant a seed. Exiting in 3 seconds..."
