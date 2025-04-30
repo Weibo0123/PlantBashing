36d12dadb1e898cbe7ce3dbf1131790fe2aff190
Weibo0123 <Weibolin322@gmail.com>
2025-04-30 14:46:01 -0500

    orgize the code

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -8,18 +8,20 @@ echo "Hello, $name! Nice to meet you"
 
 sleep 5
 
-while true; do
+ex=false
+while [ "$ex" = false ]
+do
 echo "Do you want to plant a new seed? (yes/no)"
 read choice
 
-if [ "$choice" == "yes" ]; then
+if [ "$choice" = "yes" ]; then
     # If the user chooses 'yes', let them know they have planted a seed
     echo "You have dug a hole and planted a small seed the size of a grain of rice."
     sleep 3
     echo "In the Computer's digital world, the ime moves mush faster than it does outside in the reality.Do you want to wait for one day?"
     read wait
 
-    if [ "$wait" == "yes" ]; then
+    if [ "$wait" = "yes" ]; then
         echo "wait for one days"
         sleep 3
         echo "Nothing Happen, do you want to wait for one more days?"
@@ -27,19 +29,19 @@ if [ "$choice" == "yes" ]; then
         if [[ "$wait2" == "yes" ]]; then
             echo "The seed germinated over night!"
         else 
-            break
+            ex=true
         fi
-    elif [ "$wait" == "no" ]; then
+    elif [ "$wait" = "no" ]; then
     echo "You chose not to wait. Exiting in 3 seconds..."
     sleep 3
-    break
+    ex=true
 fi
 
-    break
-elif [ "$choice" == "no" ]; then
+    ex=true
+elif [ "$choice" = "no" ]; then
     echo "You chose not to plant a seed. Exiting in 3 seconds..."
     sleep 3
-    break
+    ex=true
 else
 	echo "You can only anwser yes or no"
 fi
